0470b8a4fda262152854766da8724f29701abc07
Kalin Ringkvist <kalin@kalinflash.com>
2014-07-13 18:02:39 -0700

    completed challenge 2 and a little cleanup

--- jquery.challenge.js
+++ jquery.challenge.js
@@ -1,14 +1,38 @@
 (function($) {
   $.fn.challenge = function() {
-	$.fn.challenge.sortAndAddItems = function(aItems, sElementID) {
-      aItems = $.fn.challenge.sortItems(aItems);
-      $.fn.challenge.showItems(aItems, sElementID);
-    };
-    
+	
+	/**
+	 * sort the names in an array
+	 * @param {Array} aItems array of name strings
+	 * @return {Array}
+     */
     $.fn.challenge.sortItems = function( aItems ) {
+      aItems.sort($.fn.challenge.fullNameSortFunc)
       return aItems;
     };
     
+    /**
+     * pass this function into array.sort() to alphebatize a list of names
+     * @param {String} a 
+     * @param {String} b
+     * @return {Boolean}
+     */
+    $.fn.challenge.fullNameSortFunc = function( a, b ){
+      var aIndex = a.lastIndexOf(" ") + 1;
+      var bIndex = b.lastIndexOf(" ") + 1;
+      var A = a.charAt(aIndex).toUpperCase();
+      var B = b.charAt(bIndex).toUpperCase();
+      
+      //TODO: check if A===B and if so, loop further into string until they are different
+      return A>B;
+    }
+    
+    /**
+     * display a list of strings in a UL
+     * @param {Array} aItems array of srtings
+     * @param {String} sElementID the ID of the UL element that will contain the list items
+     * @return {null}
+     */
     $.fn.challenge.showItems = function( aItems, sElementID ) {
       var l = aItems.length;
       for(var i=0; i<l; i++){
